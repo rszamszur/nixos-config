908e95212d0802e2af2f8de705236c7f1b071aa7
Radosław Szamszur <radoslawszamszur@gmail.com>
2022-03-29 17:13:05 +0200

    Add awesome wm module

---
+++ modules/awesome/default.nix
@@ -0,0 +1,26 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.my.awesome;
+in
+{
+    options.my.awesome.enable = lib.mkEnableOption "Enable common awesome window manager options.";
+
+    config = lib.mkIf cfg.enable {
+
+        services.xserver.enable = true;
+        services.xserver.windowManager.awesome.enable = true;
+        services.udev.packages = [ pkgs.gnome3.gnome-settings-daemon ];
+        programs.dconf.enable = true;
+
+        environment.systemPackages = [
+            pkgs.gnome3.adwaita-icon-theme
+            pkgs.gnomeExtensions.appindicator
+            pkgs.gnome3.nautilus
+            pkgs.gnome3.gnome-terminal
+            pkgs.arc-icon-theme
+        ];
+
+    };
+
+}
