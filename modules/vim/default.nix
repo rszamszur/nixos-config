8b8ed6aaa69f9a8275295af427171c3736c8bdb7
Radosław Szamszur <radoslawszamszur@gmail.com>
2022-03-29 17:13:16 +0200

    Add vim module

---
+++ modules/vim/default.nix
@@ -0,0 +1,28 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.my.vim;
+in
+{
+    options.my.vim.enable = lib.mkEnableOption "Enable vim with plugins.";
+
+    config = lib.mkIf cfg.enable {
+
+        home-manager.users.rszamszur = { ... }: {
+
+            programs.vim = {
+                enable = true;
+                plugins = [
+                    pkgs.vimPlugins.YouCompleteMe
+                    pkgs.vimPlugins.syntastic
+                    pkgs.vimPlugins.vim-flake8
+                    pkgs.vimPlugins.nerdtree
+                    pkgs.vimPlugins.vim-airline
+                    pkgs.vimPlugins.gruvbox
+                ];
+            };
+
+        };
+
+    };
+}
