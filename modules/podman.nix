883202d41ffa2f99e6ecdb491a24bf45d79d37c3
Radosław Szamszur <radoslawszamszur@gmail.com>
2022-03-29 17:14:31 +0200

    Add podman module

---
+++ modules/podman.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.my.podman;
+in
+{
+
+  options.my.podman.enable = lib.mkEnableOption "Enables global settings required by podman.";
+
+  config = lib.mkIf cfg.enable {
+    virtualisation.podman.enable = true;
+  };
+
+}
