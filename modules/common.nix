ba26a6d5b0b95de8a948a56a4330371da2c131d5
Radosław Szamszur <radoslawszamszur@gmail.com>
2022-03-29 17:13:45 +0200

    Add common module

---
+++ modules/common.nix
@@ -0,0 +1,50 @@
+{ config, lib, pkgs, ... }:
+
+let
+  passHash = "$6$5UsMsT9nvv3Xy3Tl$wO/sQaNSqOUxjymXDYEeuWCsH0lZYwJhXh3sbBucUmr./CWNeWXRmJ5yqEj9pMC/jye9Vha7G5/YD1gH9dLpP1";
+  rootPassHash = "$6$DBH/m/TvG3cIKJUr$v5TjDD4RRQhrCbOZs3ktWj6v1Vz81z0KIMzlBVq6UfG8dlFkTmKLl.s3QFW4CwLIdeZmDHyof7Iufx.8VhGOk0";
+in
+{
+  environment.systemPackages = [
+        pkgs.vim
+        pkgs.wget
+        pkgs.curl
+        pkgs.firefox
+        pkgs.git
+        pkgs.vlc
+        pkgs.okular
+        pkgs.htop
+        pkgs.tmux
+        pkgs.jq
+        pkgs.dnsutils
+        pkgs.cryptsetup
+  ];
+
+  networking.networkmanager.enable = true;
+
+  services.openssh.enable = true;
+
+  users = {
+    mutableUsers = false;
+
+    users = {
+      rszamszur = {
+        uid = 1001;
+        isNormalUser = true;
+        description = "Radoslaw Szamszur";
+        home = "/home/rszamszur";
+        createHome = true;
+        group = "users";
+        extraGroups = [ "networkmanager" "wheel" ];
+        hashedPassword = passHash;
+      };
+
+      root.hashedPassword = rootPassHash;
+    };
+  };
+
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+  };
+
+}
