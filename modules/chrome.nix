9cd40ee5b9160320f3be31ba15d3d475372ff468
Radosław Szamszur <radoslawszamszur@gmail.com>
2022-03-29 17:13:32 +0200

    Add chromium module

---
+++ modules/chrome.nix
@@ -0,0 +1,46 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.my.chrome;
+in
+{
+    options.my.chrome.enable = lib.mkEnableOption "Enable chomium browser with plugins.";
+
+    config = lib.mkIf cfg.enable {
+
+        home-manager.users.rszamszur = { ... }: {
+
+            programs.chromium = {
+                enable = true;
+                extensions = [
+                    {
+                    id = "eggkanocgddhmamlbiijnphhppkpkmkl";
+                    version = "1.4.141";
+                    }
+                    {
+                    id = "hjobfcedfgohjkaieocljfcppjbkglfd";
+                    version = "1.2.1";
+                    }
+                    {
+                    id = "kbfnbcaeplbcioakkpcpgfkobkghlhen";
+                    version = "14.1054.0";
+                    }
+                    {
+                    id = "fgmjlmbojbkmdpofahffgcpkhkngfpef";
+                    version = "1.1.2";
+                    }
+                    {
+                    id = "nhdogjmejiglipccpnnnanhbledajbpd";
+                    version = "6.1.3";
+                    }
+                    {
+                    id = "gighmmpiobklfepjocnamgkkbiglidom";
+                    version = "4.44.0";
+                    }
+                ];
+            };
+
+        };
+
+    };
+}
