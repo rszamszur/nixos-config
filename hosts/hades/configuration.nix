c425312e49727fa93a2e0e589a3eee3218520bc0
Radosław Szamszur <radoslawszamszur@gmail.com>
2022-03-29 23:10:02 +0200

    Add hades host configuration

---
+++ hosts/hades/configuration.nix
@@ -0,0 +1,40 @@
+{ ... }:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ../../modules
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = [ "ntfs" ];
+
+  networking.hostName = "hades";
+
+  time.timeZone = "Europe/Warsaw";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp6s0.useDHCP = true;
+  networking.interfaces.enp7s0.useDHCP = true;
+  networking.interfaces.wlp5s0.useDHCP = true;
+
+  my.laptop.enable = true;
+  my.chrome.enable = true;
+  my.vim.enable = true;
+  my.podman.enable = true;
+  my.vscode.enable = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.11"; # Did you read the comment?
+
+}
