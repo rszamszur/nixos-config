ed979c8a3ac5642b902ad0e56a38bfefa0d24722
Radosław Szamszur <radoslawszamszur@gmail.com>
2022-04-05 20:01:10 +0200

    Add ruby shell config

---
+++ shells/ruby.nix
@@ -0,0 +1,23 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.ruby
+    pkgs.bundler
+    pkgs.sqlite
+    pkgs.postgresql
+    pkgs.libxml2
+    pkgs.libxslt
+    pkgs.pkg-config
+    pkgs.shared-mime-info
+    pkgs.gnumake
+    pkgs.cacert
+  ];
+  shellHook = ''
+    export GEM_HOME=$PWD/.gems
+    export GEM_PATH=$GEM_HOME
+    export PATH=$GEM_PATH/bin:$PATH
+    export FREEDESKTOP_MIME_TYPES_PATH="${pkgs.shared-mime-info}/share/mime"
+    export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+  '';
+}
