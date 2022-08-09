9f70df1caacf0d2033f9683e78ea76697f03b143
Radosław Szamszur <radoslawszamszur@gmail.com>
2022-08-09 19:56:54 +0200

    Fix POETRY_HOME env variable

--- shells/py3-poetry.nix
+++ shells/py3-poetry.nix
@@ -4,14 +4,15 @@
 
 let
   pythonPackage = builtins.getAttr (python) pkgs;
+  poetry = pkgs.poetry.override { python = pythonPackage; };
 in
 pkgs.mkShell {
   buildInputs = [
     pythonPackage
-    (pkgs.poetry.override { python = pythonPackage; })
+    poetry
   ];
   shellHook = ''
-    export POETRY_HOME=${pkgs.poetry}
+    export POETRY_HOME=${poetry}
     export POETRY_VIRTUALENVS_IN_PROJECT=true
     unset SOURCE_DATE_EPOCH
   '';
