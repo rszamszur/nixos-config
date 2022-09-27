caa51f43e9842622fdf0c5efe44f9deeb2c2d9d5
Radosław Szamszur <radoslawszamszur@gmail.com>
2022-09-27 16:07:56 +0200

    Use poetry2nix from source, add minor formatting changes

--- overlays/poetry2nix.nix
+++ overlays/poetry2nix.nix
@@ -1,27 +1,36 @@
-self: super: {
+final: prev:
+let
+  src = final.fetchFromGitHub {
+    owner = "nix-community";
+    repo = "poetry2nix";
+    rev = "1.31.0";
+    sha256 = "06psv5mc7xg31bvjpg030mwnk0sv90cj5bvgsdmcwicifpl3k3yj";
+  };
+  p2n = import "${src.out}/default.nix" { pkgs = final; poetry = final.poetry; };
+in
+{
+  # p2n-final & p2n-prev refers to poetry2nix
+  poetry2nix = p2n.overrideScope' (p2n-final: p2n-prev: {
 
-  # p2self & p2super refers to poetry2nix
-  poetry2nix = super.poetry2nix.overrideScope' (p2nixself: p2nixsuper: {
+    # py-final & py-prev refers to python packages
+    defaultPoetryOverrides = p2n-prev.defaultPoetryOverrides.extend (py-final: py-prev: {
 
-    # pyself & pysuper refers to python packages
-    defaultPoetryOverrides = p2nixsuper.defaultPoetryOverrides.extend (pyself: pysuper: {
-
-      mypy = pysuper.mypy.overridePythonAttrs (old: {
+      mypy = py-prev.mypy.overridePythonAttrs (old: {
         patches = [ ];
         # Compile mypy with mypyc, which makes mypy about 4 times faster. The compiled
         # version is also the default in the wheels on Pypi that include binaries.
         # is64bit: unfortunately the build would exhaust all possible memory on i686-linux.
-        MYPY_USE_MYPYC = self.stdenv.buildPlatform.is64bit;
+        MYPY_USE_MYPYC = final.stdenv.buildPlatform.is64bit;
         # If one wants to use pure python mypy, use the following:
         #MYPY_USE_MYPYC = false;
       });
-      idna = pysuper.idna.overridePythonAttrs (old: {
-        buildInputs = old.buildInputs or [ ] ++ [ pysuper.flit-core ];
+      idna = py-prev.idna.overridePythonAttrs (old: {
+        buildInputs = old.buildInputs or [ ] ++ [ py-prev.flit-core ];
       });
-      mdit-py-plugins = pysuper.mdit-py-plugins.overridePythonAttrs (old: {
-        buildInputs = old.buildInputs or [ ] ++ [ pysuper.flit-core ];
+      mdit-py-plugins = py-prev.mdit-py-plugins.overridePythonAttrs (old: {
+        buildInputs = old.buildInputs or [ ] ++ [ py-prev.flit-core ];
       });
-      suds = pysuper.suds.overridePythonAttrs (old: {
+      suds = py-prev.suds.overridePythonAttrs (old: {
         # Fix naming convention shenanigans.
         # https://github.com/suds-community/suds/blob/a616d96b070ca119a532ff395d4a2a2ba42b257c/setup.py#L648
         SUDS_PACKAGE = "suds";
@@ -31,4 +40,3 @@ self: super: {
 
   });
 }
-
