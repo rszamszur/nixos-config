483970267925c88e0cc5c5ebbd8480c211ad9084
Radosław Szamszur <radoslawszamszur@gmail.com>
2022-11-16 12:42:20 +0100

    Add tyr host

---
+++ hosts/tyr/configuration.nix
@@ -0,0 +1,47 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ../../modules
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "tyr";
+
+  time.timeZone = "Europe/Warsaw";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+
+  my.awesome.enable = true;
+  my.laptop.enable = true;
+  my.bash = {
+    enable = true;
+    homepkgs = [
+      pkgs.firefox
+      pkgs.keepassxc
+      pkgs.kubectl
+      pkgs.kubernetes-helm
+    ];
+  };
+  my.vim.enable = true;
+  my.podman.enable = true;
+  my.vscode.enable = true;
+  my.remarkable.enable = true;
+  my.chrome.enable = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05"; # Did you read the comment?
+
+}
