bbf320bc5274a716b897104dc752ebf055b22dfd
Radosław Szamszur <github@rsd.sh>
2023-03-15 16:58:16 +0100

    Add rszamszur-nixos cache

--- modules/cache.nix
+++ modules/cache.nix
@@ -12,10 +12,12 @@ in
         "https://cache.nixos.org/"
         "https://nix-community.cachix.org"
         "https://fastapi-mvc.cachix.org"
+        "https://rszamszur-nixos.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "fastapi-mvc.cachix.org-1:knQ8Qo41bnhBmOB6Sp0UH10EV76AXW5o69SbAS668Fg="
+        "rszamszur-nixos.cachix.org-1:OOpiY87os0SYfYVQmLzxTvvn2sEoeOkKzaeguQCZVyQ="
       ];
     };
     environment.systemPackages = [ pkgs.cachix ];
