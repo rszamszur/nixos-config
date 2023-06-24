f628e53b262e7c6a762d3070646d4cf1a3675b9c
Radosław Szamszur <github@rsd.sh>
2023-06-24 13:02:52 +0200

    Update to nixos 23.05

--- flake.nix
+++ flake.nix
@@ -13,9 +13,9 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     home-manager = {
-      url = "github:nix-community/home-manager/release-22.11";
+      url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-parts.url = "github:hercules-ci/flake-parts";
