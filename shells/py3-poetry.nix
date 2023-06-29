de2b0b0a9f0faac61af76ce3acfcf039dd1ee742
Radosław Szamszur <github@rsd.sh>
2023-06-29 11:50:42 +0200

    Update argument name

--- shells/py3-poetry.nix
+++ shells/py3-poetry.nix
@@ -4,7 +4,7 @@
 
 let
   pythonPackage = builtins.getAttr (python) pkgs;
-  poetry = pkgs.poetry.override { python = pythonPackage; };
+  poetry = pkgs.poetry.override { python3 = pythonPackage; };
 in
 pkgs.mkShell {
   buildInputs = [
