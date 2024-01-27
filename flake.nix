116c03fe918acb8ba15ebd8413516f20eac5af5b
Radosław Szamszur <radoslawszamszur@gmail.com>
2024-01-27 15:04:03 +0100

    chore: updates to NixOS 23.11

--- flake.nix
+++ flake.nix
@@ -13,9 +13,9 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-parts.url = "github:hercules-ci/flake-parts";
