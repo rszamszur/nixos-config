afb3880f26fbd513c57e5d42775cc42c803927b4
Radosław Szamszur <github@rsd.sh>
2024-04-05 13:41:53 +0200

    feat: init and configure secrets for tyr hosts

--- hosts/tyr/configuration.nix
+++ hosts/tyr/configuration.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   imports = [
@@ -30,10 +30,41 @@
   # Run unpatched dynamic binaries on NixOS
   programs.nix-ld.enable = true;
 
+  # Sops secrets
+  sops.age.keyFile = "/var/lib/sops-nix/key.txt";
+  sops.age.sshKeyPaths = [ "/etc/ssh/sopsnix_ed25519" ];
+  sops.age.generateKey = true;
+  sops.secrets.gh-runners-token = {
+    sopsFile = ./secrets/gh-runners.yaml;
+    restartUnits = [
+      "github-runner-pve-nixos-tyr-runner1.service"
+      "github-runner-pve-nixos-tyr-runner2.service"
+    ];
+  };
+  sops.secrets.binary-cache-key = {
+    sopsFile = ./secrets/remote-builder.yaml;
+    owner = "nixremote";
+    group = "nixremote";
+    mode = "0600";
+  };
+
+  # My NixOS modules
   my.cache.enable = true;
   my.bash.enable = true;
   my.vim.enable = true;
   my.podman.enable = true;
+  my.github-runners = {
+    enable = true;
+    namePrefix = "nixos-tyr";
+    tokenFile = config.sops.secrets.gh-runners-token.path;
+  };
+  my.remote-builder = {
+    enable = true;
+    authorizedKeys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCrV5PuqdHzkTDzoWB3JA6377lNzPooNx+Rt4Bx9CkZP1uCiGQzvdrfGcBCkWeJGQcyP2J0AqJ+wJjdQKyrOfHXXPXkwvaudReU/wIc0FSVbOGr3bsa2FF4mHhezW+0N4FHWpFpqlhp4cGY5Lw9nT8J6s0mEe3Z+VzaGhAETvJON+rDtl9Z8IhRJ2QvcTDGkh2rHvF5+87hmzIgCzyWTUNX8VTunYiznBGCFo7N4TRoF+N1RPZM3AGsUuVwI00iOkgFdK5cXy2N3bomHNuFH8jvinp0g2jZUw2pRFlIuN95obPvdkAPnAsSM+iqKG37kFBhqkPM5bup9+dM1pRynnAIjiuOAQGRPJJfGJUt+dF1KuJqQ0VNLOPW0Gkq7eK1Q5AhrI7dTd4e9edUTGh3If4HIqDyU7+LaxjkB6lsXW6JcYE5dQed9cgihWi4iWHosiDdQmx5TTfZTjk/VtbAUV3dGHjq8ScObz+FU/V6tEPI6awfTCpFOMxUW+CuPbG/PPFZjaR0iNcuiFyP1+Wx9EyBYHEY5eW4Z1PrkhQ/yKKGhcJmUwCM+4GbjO0WgSqMQBacyA/L2Nz102SZbzmEw/UMA9ly8MBrgo+dXDpZUJs6QPAWsxPg9uXURyyB7LGfnSilKTJoNNZvqH/YY1pWcE9uLGfl3q3cGJIYbt668BChLw== rszamszur@fenrir"
+    ];
+    binaryCacheKey = config.sops.secrets.binary-cache-key.path;
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
