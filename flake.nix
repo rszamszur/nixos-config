fe62bbc4b13d4d66e7fd14c53399262884c80382
Radosław Szamszur <github@rsd.sh>
2024-07-09 21:32:34 +0200

    chore: updates nixpkgs to nixos-24.05

--- flake.nix
+++ flake.nix
@@ -13,9 +13,9 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sops-nix = {
