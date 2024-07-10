8b1bd83543847ef0cd12a0a6f53c034311cc61bf
Radosław Szamszur <github@rsd.sh>
2024-07-10 19:13:52 +0200

    fix: build rmview with Python 3.11

--- pkgs/flake-module.nix
+++ pkgs/flake-module.nix
@@ -6,7 +6,7 @@
       manix = pkgs.callPackage ./tools/nix/manix { };
       cups-remarkable = pkgs.callPackage ./misc/cups/drivers/remarkable { };
       rmview = pkgs.callPackage ../pkgs/applications/misc/remarkable/rmview {
-        python3Packages = pkgs.python39Packages;
+        python3Packages = pkgs.python311Packages;
         wrapQtAppsHook = pkgs.qt5.wrapQtAppsHook;
       };
     };
