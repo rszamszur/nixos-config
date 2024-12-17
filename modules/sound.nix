b0bb1646c0a93d0a22fb3cb0be50cbc8be842c94
Radosław Szamszur <github@rsd.sh>
2024-12-18 00:07:54 +0100

    fix: address NixOS 24.11 deprecations and changes

--- modules/sound.nix
+++ modules/sound.nix
@@ -5,32 +5,61 @@ let
 in
 {
 
-  options.my.sound.enable = lib.mkEnableOption "Enables common sound settings.";
+  options.my.sound = {
+    enable = lib.mkEnableOption "Enables common sound settings.";
+    driver = lib.mkOption {
+      type = lib.types.enum [
+        "pulseaudio"
+        "pipewire"
+      ];
+      default = "pipewire";
+      description = ''
+        What sound server to use, defaults to pipewire.
+      '';
+    };
+  };
 
   config = lib.mkIf cfg.enable {
-    sound.enable = true;
     hardware = {
-      pulseaudio = {
-        enable = true;
-        package = pkgs.pulseaudioFull;
-      };
       bluetooth = {
         enable = true;
         powerOnBoot = true;
-        hsphfpd.enable = true;
         settings = {
           General = {
             Enable = "Source,Sink,Media,Socket";
           };
         };
       };
+    } // lib.optionalAttrs
+      (cfg.driver == "pulseaudio")
+      {
+        pulseaudio = {
+          enable = true;
+          package = pkgs.pulseaudioFull;
+        };
+        bluetooth = {
+          hsphfpd.enable = true;
+        };
+      };
+
+    services.pipewire = lib.mkIf (cfg.driver == "pipewire") {
+      enable = true; # if not already enabled
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
+      wireplumber.enable = true;
     };
+
     services.blueman.enable = true;
     programs.noisetorch.enable = true;
 
     environment.systemPackages = [
       pkgs.pavucontrol
     ];
+
+    security.rtkit.enable = true;
   };
 
 }
--- pkgs/tools/nix/manix/default.nix
+++ pkgs/tools/nix/manix/default.nix
@@ -11,7 +11,7 @@ rustPlatform.buildRustPackage rec {
     sha256 = "0lgynk257yclrpwwmrz95wy0i1a8m7fjxl6pqc4vgrvds00aq3bb";
   };
 
-  cargoSha256 = "0l0kb0j5zyvq78mgynx4f4blz9g9sj1wdzpc7z9vw3a7i8150hil";
+  cargoHash = "sha256-NEJQAopHDb7TP+z+xoPU6aVPF3GkW/8qOnj7XyRYE1A=";
 
   meta = with lib; {
     description = "A fast CLI documentation searcher for Nix.";
