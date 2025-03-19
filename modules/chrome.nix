61dec02f022d2c99e33fd2da0d337674fd1e2bd9
Radosław Szamszur <github@rsd.sh>
2025-03-19 14:28:49 +0100

    chore(nixos-chrome): updates extensions and default package

--- modules/chrome.nix
+++ modules/chrome.nix
@@ -1,41 +1,46 @@
-{ config, lib, ... }:
+{ config, pkgs, lib, ... }:
 
 let
   cfg = config.my.chrome;
 in
 {
-  options.my.chrome.enable = lib.mkEnableOption "Enable chomium browser with plugins.";
+  options.my.chrome = {
+    enable = lib.mkEnableOption "Enable chomium browser with plugins.";
+    package = lib.mkPackageOption pkgs "ungoogled-chromium" { };
+  };
 
   config = lib.mkIf cfg.enable {
 
     home-manager.users.rszamszur = { ... }: {
 
       programs.chromium = {
+        inherit (cfg) package;
         enable = true;
         extensions = [
           {
+            # Tabs Outliner
             id = "eggkanocgddhmamlbiijnphhppkpkmkl";
-            version = "1.4.141";
+            version = "1.4.153";
           }
           {
-            id = "hjobfcedfgohjkaieocljfcppjbkglfd";
-            version = "1.2.1";
-          }
-          {
+            # Grammarly
             id = "kbfnbcaeplbcioakkpcpgfkobkghlhen";
-            version = "14.1054.0";
+            version = "14.1227.1";
           }
           {
+            # Start page
             id = "fgmjlmbojbkmdpofahffgcpkhkngfpef";
-            version = "1.1.2";
+            version = "3.0.7";
           }
           {
+            # Vue.js devtools
             id = "nhdogjmejiglipccpnnnanhbledajbpd";
-            version = "6.1.3";
+            version = "7.7.0";
           }
           {
-            id = "gighmmpiobklfepjocnamgkkbiglidom";
-            version = "4.44.0";
+            # Ublock
+            id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";
+            version = "1.63.0";
           }
         ];
       };
