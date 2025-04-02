d7b44abb5f340da75346232bf703f0b92fa95b58
Radosław Szamszur <github@rsd.sh>
2025-04-02 09:03:00 +0200

    feat(home): implements rszamszur home configuration

--- flake.nix
+++ flake.nix
@@ -176,6 +176,12 @@
               ./home/coder.nix
             ];
           };
+          rszamszur = home-manager.lib.homeManagerConfiguration {
+            pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+            modules = [
+              ./home/rszamszur.nix
+            ];
+          };
         };
       };
     };
