1f17e7d09b23f8fd6a1dd58bf3bfc9067571c307
Radosław Szamszur <github@rsd.sh>
2025-04-06 13:14:58 +0200

    feat(fenrir): implements fenrir host install script

---
+++ hosts/fenrir/install.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash
+#! nix-shell -p git
+#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/nixos-23.11.tar.gz
+
+if [ -n "$DEBUG" ]; then
+    set -x
+fi
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+TARGET_DRIVE=$1
+NIXOS_HOST="fenrir"
+BOOT_SIZE=${BOOT_SIZE:=1000M}
+
+# EFI system partition on a GUID Partition Table is identified by the partition type GUID C12A7328-F81F-11D2-BA4B-00A0C93EC93B
+sfdisk "$TARGET_DRIVE" << EOF
+label: gpt
+,$BOOT_SIZE,C12A7328-F81F-11D2-BA4B-00A0C93EC93B
+;
+EOF
+
+mkfs.vfat -n boot "$TARGET_DRIVE"1
+mkfs.btrfs -L root "$TARGET_DRIVE"2
+mount "$TARGET_DRIVE"2 /mnt
+mkdir /mnt/boot
+mount "$TARGET_DRIVE"1 /mnt/boot
+
+nixos-generate-config --root /mnt
+
+mkdir /mnt/var
+pushd /mnt/var
+git clone https://github.com/rszamszur/nixos-config.git
+popd
+
+pushd /mnt/etc/nixos
+mv configuration.nix configuration.generated.nix
+ln -s ../../var/nixos-config/hosts/$NIXOS_HOST/configuration.nix configuration.nix
+mv hardware-configuration.nix /mnt/var/nixos-config/hosts/$NIXOS_HOST
+ln -s ../../var/nixos-config/hosts/$NIXOS_HOST/hardware-configuration.nix hardware-configuration.nix
+popd
+
+nixos-install --no-root-password --flake /mnt/var/nixos-config#$NIXOS_HOST
