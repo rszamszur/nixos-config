e4c73f8ab589db6343ca0b5565f96bea12e41c56
Radosław Szamszur <github@rsd.sh>
2025-10-16 22:28:46 +0200

    feat(cache): implements cache upload post-build-hook

--- modules/cache.nix
+++ modules/cache.nix
@@ -2,6 +2,30 @@
 
 let
   cfg = config.my.cache;
+  buildCacheUploadHook = key: pkgs.writeShellApplication {
+    name = "nixgard-upload";
+    runtimeInputs = [ pkgs.nix ];
+    runtimeEnv = {
+      IFS = " ";
+      SIGN_KEY_PATH = key;
+    };
+    bashOptions = [
+      "errexit"
+      "nounset"
+      "noglob"
+    ];
+    excludeShellChecks = [
+      "SC2086"
+    ];
+    text = ''
+      if [[ -n "$OUT_PATHS" ]]; then
+        echo "Signing paths" $OUT_PATHS
+        nix store sign -k "$SIGN_KEY_PATH" $OUT_PATHS
+        echo "Uploading to cache: $OUT_PATHS"
+        exec nix copy --to 'http://nixgard.szamszur.cloud' $OUT_PATHS -vvv
+      fi
+    '';
+  };
 in
 {
   options.my.cache = {
@@ -16,20 +40,28 @@ in
       default = [ ];
       description = "List of extra trusted public keys.";
     };
+    binaryCacheKey = lib.mkOption {
+      type = lib.types.nullOr lib.types.path;
+      default = null;
+      description = lib.mdDoc ''
+        The full path to a file which contains the binary cache private key.
+      '';
+    };
   };
 
   config = lib.mkIf cfg.enable {
-    nix.settings = {
-      substituters = [
-        "https://nix-community.cachix.org"
-        "https://rszamszur-nixos.cachix.org"
-      ] ++ cfg.extraSubstituters;
-      trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "rszamszur-nixos.cachix.org-1:OOpiY87os0SYfYVQmLzxTvvn2sEoeOkKzaeguQCZVyQ="
-      ] ++ cfg.extraTrustedPublicKeys;
+    nix = {
+      settings = {
+        substituters = [
+          "http://nixgard.szamszur.cloud"
+        ] ++ cfg.extraSubstituters;
+        trusted-public-keys = [
+          "nixgard.szamszur.cloud:HaXsNyMojj3pVViZDoH8n9uJgqGcoZ6V1yYIFSigOxY="
+        ] ++ cfg.extraTrustedPublicKeys;
+      };
+      extraOptions = lib.mkIf (cfg.binaryCacheKey != null) ''
+        post-build-hook = ${buildCacheUploadHook cfg.binaryCacheKey}/bin/nixgard-upload
+      '';
     };
-    environment.systemPackages = [ pkgs.cachix ];
   };
-
 }
