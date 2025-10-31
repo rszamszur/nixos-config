c91acd343b3e2c98ebbed0c18b626d9ccfba859e
Radosław Szamszur <github@rsd.sh>
2025-10-31 17:11:42 +0100

    feat(modules): implements remote gaming NixOS module

--- modules/docker.nix
+++ modules/docker.nix
@@ -4,24 +4,35 @@ let
   cfg = config.my.docker;
 in
 {
-
-  options.my.docker.enable = lib.mkEnableOption "Enables global settings required by docker.";
+  options.my.docker = {
+    enable = lib.mkEnableOption "Enables global settings required by docker.";
+    enableNvidia = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable nvidia-docker wrapper, supporting NVIDIA GPUs inside docker containers.";
+    };
+    extraDockerGroupUsers = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      description = lib.mdDoc "Adds extra users to the docker group.";
+      default = [ ];
+    };
+  };
 
   config = lib.mkIf cfg.enable {
-
     virtualisation.docker = {
       enable = true;
       storageDriver = "btrfs";
     };
 
-    users.extraGroups.docker.members = [ "rszamszur" ];
+    hardware.nvidia-container-toolkit.enable = cfg.enableNvidia;
+    virtualisation.docker.daemon.settings.features.cdi = cfg.enableNvidia;
+    virtualisation.docker.rootless.daemon.settings.features.cdi = cfg.enableNvidia;
+
+    users.extraGroups.docker.members = [ "rszamszur" ] ++ cfg.extraDockerGroupUsers;
 
     environment.systemPackages = [
-      pkgs.minikube
-      pkgs.kubectl
-      pkgs.kubernetes-helm
+      pkgs.dive
+      pkgs.trivy
     ];
-
   };
-
 }
