d314ae62a90c75201638d0dfe4e41cd542fd90b8
Radosław Szamszur <github@rsd.sh>
2026-02-08 12:01:43 +0100

    feat(github-runners): enables configuring multiple runners

--- modules/github-runners.nix
+++ modules/github-runners.nix
@@ -5,24 +5,36 @@ let
   cfg = config.my.github-runners;
 in
 {
-  options.my.github-runners = filterRemovedOptions (options.services.github-runners.type.getSubOptions [ config.my.github-runners.name.value ]) // {
+  options.my.github-runners = {
     enable = lib.mkEnableOption "Enable self-hosted GitHub runners.";
-    name = lib.mkOption {
-      type = lib.types.str;
-      default = "nixos-runner";
-      description = lib.mdDoc ''
-        GitHub runner name.
-      '';
+    runners = lib.mkOption {
+      type = lib.types.attrsOf (lib.types.submodule (
+        { name, config, ... }:
+        {
+          options = (inheritAllExcept (filterRemovedOptions (options.services.github-runners.type.getSubOptions { inherit name config; })) [ "enable" "name" "_module" ]) // {
+            name = lib.mkOption {
+              type = lib.types.str;
+              default = "nixos-runner";
+              description = lib.mdDoc ''
+                GitHub runner name.
+              '';
+            };
+            enable = lib.mkOption {
+              default = true;
+              example = true;
+              description = "Whether to enable GitHub Actions runner.";
+              type = lib.types.bool;
+            };
+          };
+        }
+      )
+      );
     };
   };
 
   config = lib.mkIf cfg.enable {
 
-    services.github-runners = {
-      "${cfg.name}" = inheritAllExcept cfg [ "enable" ] // {
-        enable = true;
-      };
-    };
+    services.github-runners = cfg.runners;
 
   };
 
