6e4be1ef755aef4086708bc95cdfa25b1124a83b
Radosław Szamszur <github@rsd.sh>
2026-05-09 08:52:27 +0200

    chore(gaming): adds gamescope

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -41,8 +41,10 @@ in
       package = pkgs.steam;
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
       dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      gamescopeSession.enable = true;
     };
     hardware.steam-hardware.enable = true;
+    programs.gamemode.enable = true;
 
     environment.systemPackages = [
       pkgs.pkgs.moonlight-qt
